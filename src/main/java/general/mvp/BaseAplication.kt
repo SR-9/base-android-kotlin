335705bd9fcae8e8252e2c2151f99d31b6da6521
VanHieu <tvhieuit@gmail.com>
2018-02-02 21:46:21 +0700

    - multidex

---
+++ src/main/java/general/mvp/BaseAplication.kt
@@ -0,0 +1,14 @@
+package general.mvp
+
+import android.support.multidex.MultiDexApplication
+
+
+/**
+ * ____________________________________
+ *
+ * Generator: Hieu.TV - tvhieuit@gmail.com
+ * CreatedAt: 2/2/18
+ * ____________________________________
+ */
+
+class BaseAplication : MultiDexApplication()
