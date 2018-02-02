37b8da55f4aaf0a7cd02b4b276aacb1183d81f56
VanHieu <tvhieuit@gmail.com>
2018-02-03 01:47:28 +0700

    - material view

---
+++ src/main/java/general/widget/MaterialTextView.kt
@@ -0,0 +1,23 @@
+package general.widget
+
+import android.content.Context
+import android.support.v7.widget.AppCompatTextView
+import android.util.AttributeSet
+
+
+/**
+ * ____________________________________
+ *
+ * Generator: Hieu.TV - tvhieuit@gmail.com
+ * CreatedAt: 2/3/18
+ * ____________________________________
+ */
+
+
+class MaterialTextView @JvmOverloads constructor(
+	context : Context,
+	attributeSet : AttributeSet? = null,
+	defAttrStyle : Int = 0
+) : AppCompatTextView(context, attributeSet, defAttrStyle) {
+
+}
