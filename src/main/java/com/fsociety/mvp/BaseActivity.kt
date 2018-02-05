29a9e670613a291bcd1eb3ccee8b68820e7c2914
Van Hieu <tvhieuit@gmail.com>
2018-02-05 16:42:36 +0700

    refactor

--- src/main/java/com/fsociety/mvp/BaseActivity.kt
+++ src/main/java/com/fsociety/mvp/BaseActivity.kt
@@ -1,6 +1,9 @@
 package com.fsociety.mvp
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.baseappication.R
 import com.trello.rxlifecycle2.components.support.RxAppCompatActivity
 import com.fsociety.widget.Loading
 
@@ -17,6 +20,23 @@ abstract class BaseActivity : RxAppCompatActivity() {
 	override fun onCreate(savedInstanceState : Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(layoutId)
+		setupActionbar()
 		onViewCreated()
+	}
+
+	private fun setupActionbar() {
+		val actionBar = supportActionBar
+		actionBar?.let {
+			title = "Fuck Society"
+		}
+	}
+
+	override fun onCreateOptionsMenu(menu : Menu?) : Boolean {
+		menuInflater.inflate(R.menu.main_menu, menu)
+		return super.onCreateOptionsMenu(menu)
+	}
+
+	override fun onContextItemSelected(item : MenuItem?) : Boolean {
+		return super.onContextItemSelected(item)
 	}
 }
